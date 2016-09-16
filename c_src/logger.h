4acc0ac0e19757516ed6a1a7b61fb04e1f81f46e
Mark Jones <markalanj@gmail.com>
2016-09-15 21:59:26 -0500

    Added copyright to each source file

--- c_src/logger.h
+++ c_src/logger.h
@@ -1,3 +1,35 @@
+/*
+ * Copyright (c) 2016, Mark Jones <markalanj@gmail.com>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * The names of its contributors may not be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef LOGGER_H
 #define LOGGER_H
 
@@ -14,9 +46,10 @@ void logger_print(const char *format, ...);
 #define LOGGER_CLOSE            logger_close
 #define LOGGER_PRINT(...)       logger_print(__VA_ARGS__)
 #else
-#define LOGGER_OPEN(x,y)        
-#define LOGGER_CLOSE()       
+#define LOGGER_OPEN(x,y)
+#define LOGGER_CLOSE()
 #define LOGGER_PRINT(...)
 #endif
 
 #endif
+
